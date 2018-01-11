5b9d2d427db15fef1409790dac5dfba6d742d2bf
Olga Mrozowicz <olga@student.agh.edu.pl>
2018-01-11 19:23:57 +0100

    zad1

--- zad1/main.cpp
+++ zad1/main.cpp
@@ -1,27 +1,44 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <cstring>
 
 int main() {
     std::fstream plik;
+    std::fstream out;
     std::string dane;
-    std::ofstream out;
     std::vector<int> cyfry;
-    int a;
-    plik.open( "in1.txt", std::ios::in | std::ios::out );
-    std::fstream plik( "out1.txt", std::ios::out );
-    if( plik.good() == true )
-    {
-        getline( plik, dane );
-        if (dane=="==") {}
-        else
-        {
-            a=dane[0];
-            if (a<=57 && a>=49) cyfry.push_back(a);
-            
+    int a, wynik = 0;
+    plik.open("in1.txt", std::ios::in | std::ios::out);
+    out.open("out1.txt", std::ios::out);
+    while (plik.good() == true) {
+        getline(plik, dane);
 
-
+        a = dane[0] - 48;
+        if (dane == "==") {
+            return 0;
+        } else if (a <= 9 && a >= 0) cyfry.push_back(a);
+        else if (dane == "=") {
+            out << wynik;
+            std::cout << wynik;
+            wynik = 0;
+        } else {
+            switch (dane[0]) {
+                case '+':
+                    if (cyfry.size()==1) cyfry.push_back(0);
+                    wynik = cyfry[cyfry.size() - 2] + cyfry[cyfry.size() - 1];
+                    cyfry.pop_back();
+                    cyfry.pop_back();
+                    cyfry.push_back(wynik);
+                    break;
+                case '-':
+                    if (cyfry.size()==1) cyfry.push_back(0);
+                    wynik = cyfry[cyfry.size() - 2] - cyfry[cyfry.size() - 1];
+                    cyfry.pop_back();
+                    cyfry.pop_back();
+                    cyfry.push_back(wynik);
+                    break;
+            }
         }
-
-    } else std::cout << "Dostep do pliku zostal zabroniony!" << std::endl;
+    }
 }
